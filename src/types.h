27758338f83f2158be873236b168817331701c1c
Roland Bernard <rolli.bernard@gmail.com>
2019-03-12 22:04:27 +0100

    Added some types for future use

---
+++ src/types.h
@@ -0,0 +1,27 @@
+// Copyright (c) 2019 Roland Bernard
+#ifndef __TYPES_H__
+#define __TYPES_H__
+
+#include <stdint.h>
+
+#define MAX_NAME_LEN 64
+#define MAX_GROUP_LEN 64
+#define MAX_TYPE_LEN 64
+#define MAX_BUFFER_LEN 64000
+
+typedef uint32_t id_t;
+typedef uint32_t len_t;
+typedef uint32_t hash_t;
+typedef uint8_t hash256_t[32];
+typedef uint8_t hash512_t[64];
+
+typedef struct {
+	id_t cid;
+	len_t len;
+	char name[MAX_NAME_LEN];	// username
+	char group[MAX_GROUP_LEN];	// groupname
+	char type[MAX_TYPE_LEN];	// e.g. ~typing, ~key:<key>
+	char data[MAX_BUFFER_LEN];
+} msgbuf_t;
+
+#endif
