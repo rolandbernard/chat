629d3d99c11e15a12c11f6c7446c2ca6ac09cdc8
Roland Bernard <rolli.bernard@gmail.com>
2019-03-15 11:10:54 +0100

    Removed tests, changed test to test the PRNG

--- src/test.c
+++ src/test.c
@@ -2,15 +2,25 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "hash.h"
+#include "random.h"
+
+#define NUM_ROLL 10000000
+#define NUM_POS 6
 
 int main(int argc, char** argv) {
 
-	hash512_t hash;
-	hash_sha512(hash, argv[1], strlen(argv[1]));
-	for(len_t i = 0; i < 64; i++)
-		fprintf(stdout, "%.2hhx", hash[i]);
-	fprintf(stdout, "\n");
+	len_t acc[NUM_POS] = { 0 };
+	random_seed_unix_urandom();
+	for(len_t i = 0; i < NUM_ROLL; i++) {
+		data256_t randdata;
+		random_get(randdata);
+		uint32_t ind = 0;
+		for(len_t j = 0; j < sizeof(randdata); j++)
+			ind ^= randdata[j] << ((j*8)%(sizeof(ind)*8));
+		acc[ind%NUM_POS]++;
+	}
+	for(len_t i = 0; i < NUM_POS; i++)
+		printf("%i: %i\n", i+1, acc[i]);
 
 	return 0;
 }
